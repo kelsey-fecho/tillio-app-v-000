fb633fc3ea702611d0a43535f06956c4a27e6f36
Kelsey Fecho <kfecho30@gmail.com>
2018-06-11 14:10:01 -0400

    Replace uid with email for oAuth logins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
   end
 
   def facebook
-    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+    @user = User.find_or_create_by(email: auth['email']) do |u|
       u.name = auth['info']['name']
       u.email = auth['info']['email']
       u.image = auth['info']['image']
