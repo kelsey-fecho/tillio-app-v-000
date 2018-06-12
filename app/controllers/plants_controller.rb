97cf8bbb55ab3bca54c1b8c38d3c3af7fe3056d7
Kelsey Fecho <kfecho30@gmail.com>
2018-06-12 13:26:43 -0400

    Add plants#create logic to controller

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -9,4 +9,15 @@ class PlantsController < ApplicationController
   def show
     @p = Plant.find(params[:id])
   end
+
+  def create
+    @plant = Plant.create(plant_params)
+    redirect_to plant_path(@plant)
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:name, :kind, :landscape_categories, :description, :zone, :height, :bloom_time, :flower_color, :light, :water, :drainage, :growth_rate, :maintenance, :susceptibility)
+  end
 end
