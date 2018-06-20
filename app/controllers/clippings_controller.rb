760bf7de80653a3944973d226629058bb676dd5c
Kelsey Fecho <kfecho30@gmail.com>
2018-06-20 12:33:55 -0400

    Add all routes for clippings to accommodate ratings

--- app/controllers/clippings_controller.rb
+++ app/controllers/clippings_controller.rb
@@ -1,11 +1,6 @@
 class ClippingsController < ApplicationController
   before_action :require_login
 
-  def new
-    @user = User.find(params[:user_id])
-    @clipping = Clipping.new(user_id: params[:user_id])
-  end
-
   def create
     Clipping.find_or_create_by(user_id: params[:user_id], plant_id: params[:clipping][:plant_id])
     redirect_to user_path(current_user)
@@ -13,6 +8,7 @@ class ClippingsController < ApplicationController
 
   def index
     if params[:user_id]
+      @user = User.find(params[:user_id])
       @clippings = User.find(params[:user_id]).clippings
     else
       @clippings = Clipping.all
