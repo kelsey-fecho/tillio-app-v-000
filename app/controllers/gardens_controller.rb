e041b30756747479b1907a1a5bf0fa493c21494b
Kelsey Fecho <kfecho30@gmail.com>
2018-07-09 17:21:36 -0400

    Add log in verification logic to gardens index page

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,6 +1,10 @@
 class GardensController < ApplicationController
 
   def index
+    if !logged_in?
+      redirect_to '/login'
+    end
+    @plants = Plant.all
     @gardens = Garden.all
     #@garden = current_user.gardens.build
   end
