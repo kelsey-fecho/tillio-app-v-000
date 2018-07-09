2c96ea55f81f72e451a753da6d8459ddf14db7bf
Kelsey Fecho <kfecho30@gmail.com>
2018-07-09 15:48:04 -0400

    Change Clipping view and controller to reflect new data structure.

--- app/controllers/clippings_controller.rb
+++ app/controllers/clippings_controller.rb
@@ -3,17 +3,16 @@ class ClippingsController < ApplicationController
 
   def create
     plant = Plant.find(params[:clipping][:plant_id])
-    Clipping.find_or_create_by(user_id: params[:user_id], plant_id: plant.id)
-    redirect_to user_clippings_path(current_user)
+    garden = Garden.find(params[:clipping][:garden_id])
+    Clipping.find_or_create_by(garden_id: garden.id, plant_id: plant.id)
+    redirect_to garden_clippings_path(garden)
   end
 
   def index
     @cuser = current_user
-    if params[:user_id]
-      @user = User.find(params[:user_id])
-      @clippings = User.find(params[:user_id]).clippings
-    else
-      @clippings = Clipping.all
+      @garden = Garden.find(params[:garden_id])
+      @user = User.find(@garden.user_id)
+      @clippings = Garden.find(params[:garden_id]).clippings
     end
   end
 
@@ -24,12 +23,12 @@ class ClippingsController < ApplicationController
   def update
     @clipping = Clipping.find(params[:id])
     @clipping.update(rating: params[:clipping][:rating])
-    redirect_to user_clippings_path(current_user)
+    redirect_to garden_clippings_path(@clipping.garden)
   end
 
   private
 
   def clipping_params
-    params.require(:clipping).permit(:id, :user_id, :plant_id, :rating)
+    params.require(:clipping).permit(:id, :garden_id, :plant_id, :rating)
   end
 end
