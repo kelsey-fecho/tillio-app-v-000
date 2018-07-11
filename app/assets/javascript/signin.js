13e880a7b04227325923cb816b02c3ddd93500b0
Kelsey Fecho <kfecho30@gmail.com>
2018-07-11 12:33:43 -0400

    Add jQuery functionality to signin/signup page

--- app/assets/javascript/signin.js
+++ app/assets/javascript/signin.js
@@ -1,5 +1,8 @@
 $(function(){
   $("a#signup").on("click", function(e){
+    $.get(this.href).success(function(response){
+      $("div#access").html(response)
+    })
     e.preventDefault();
   })
 })
